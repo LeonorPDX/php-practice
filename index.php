c1c693e4c104428a25baa7297c0459af338eae2d
LeonorPDX <leonoracolbert@gmail.com>
2020-12-21 12:53:59 -0800

    Added basic notes on variables, how to write comments, and how to interpolate variable into string

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-    echo 'Hello world from the new subdirectory';
+    // Use 2 forward-slash for a single line comment.
+
+    /* Use slash and asterix for multiple lines comment.
+    End with the asterix then slash. */
+
+    // Variables in php always start with $ 
+
+    $age = 32;
+
+    // Use double quotes to interpolate variable in a string
+
+    echo "My age is $age";
+
+    echo 'Hello World';
+    
 ?>
